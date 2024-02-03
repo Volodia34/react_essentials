34c9d16c1d25435c8a9be30c6c0e7b10cf9ce07e
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-02-03 18:29:23 +0200

    last_commit_2

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line react/prop-types
+function TabButton({children,onSelect,isSelected}) {
+
+
+    return (
+        <li><button className={isSelected ? 'active': undefined} onClick={onSelect}>{children}</button></li>
+    );
+}
+
+export default TabButton;
